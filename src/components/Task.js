00e62f88b3feb5a56c13710299b9498a9459f42a
emilio girardi <emilio.g.girardi@gmail.com>
2021-11-14 18:24:19 +0100

    install react-icons

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,10 @@
+import {FaTimes} from 'react-icons/fa'
 
 const Task = ({task}) => {
   return (
     <div className="bg-gray-100 text-black m-2 py-5 px-10 cursor-pointer">
       <h3 className="flex align-center justify-between">
-        {task.text}
+        {task.text} <FaTimes style={{ color: 'red', cursor: 'pointer' }}/>
       </h3>
       <p>{task.day}</p>
     </div>
