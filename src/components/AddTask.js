cd97402b3cc39417097ac4a331f617200a613d75
emilio girardi <emilio.g.girardi@gmail.com>
2021-11-20 17:06:17 +0100

    Add new task

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -7,8 +7,9 @@ const AddTask = ({onAdd}) => {
   const [day, setDay] = useState('')
   const [reminder, setReminder] = useState(false)
 
-  const onSubmit = (event) => { 
-    event.preventDefaut();
+  const onSubmit = (e) => { 
+
+    e.preventDefault()
 
     if(!text) { 
       alert('Please add a task!')
@@ -25,52 +26,50 @@ const AddTask = ({onAdd}) => {
   } 
 
   return (
-    <div className="w-full max-w-sm">
+    <div className='w-full max-w-sm'>
 
-      <form className="bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 mb-4" 
+      <form className='bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 mb-4' 
             onSubmit={onSubmit}
       >
 
-        <div className="mb-4">
-          <label className="block text-gray-700 text-md font-bold mb-2">
+        <div className='mb-4'>
+          <label className='block text-gray-700 text-md font-bold mb-2'>
             Task
           </label>
-          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700      leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            placeholder="Add Task"
+          <input className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700      leading-tight focus:outline-none focus:shadow-outline'
+            type='text'
+            placeholder='Add Task'
             value={text}
             onChange={ (event) => setText(event.target.value)}
           />
         </div>
 
-        <div className="mb-4">
-          <label className="block text-gray-700 text-md font-bold mb-2">
+        <div className='mb-4'>
+          <label className='block text-gray-700 text-md font-bold mb-2'>
             Day & Time
           </label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              type="text"
-              placeholder="Add Day & Time"
+            <input className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+              type='text'
+              placeholder='Add Day & Time'
               value={day}
               onChange={ (event) => setDay(event.target.value)}
             />
         </div>
 
-        <div className="mb-4">
-          <label className="block text-gray-700 text-md font-bold mb-2">
+        <div className='mb-4'>
+          <label className='block text-gray-700 text-md font-bold mb-2'>
             Set Reminder
           </label>
           <input
-            type="checkbox"
+            type='checkbox'
             checked={reminder}
             value={reminder}
             onChange={ (event) => setReminder(event.currentTarget.checked)}
           />
         </div>
 
-        <div className="flex items-center justify-between">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" type="button">
-            Save Task
-          </button>
+        <div className='flex items-center justify-between'>
+          <input type='submit' value='Save Task' className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full' />
         </div>
       </form>
     </div>
